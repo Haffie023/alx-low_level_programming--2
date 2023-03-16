22d489922845a7a2d08c317f2887d7819484c19a
Haffie023 <joyce.muchoki@gmail.com>
2023-03-16 12:25:04 -0700

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ *
+ * Return:0 on success
+ */
+
+int main(void)
+{
+	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(d));
+	printf("Size of an int: %lu byte(s)", (unsigned long)sizeof(a));
+	printf("Size of a long int: %lu byte(s)", (unsigned long)sizeof(b));
+	printf("Size of a long long int: %lu byte(s)", (unsigned long)sizeof(c));
+	printf("Size of a float: %lu byte(s)", (unsigned long)sizeof(f));
+	Return(0);
+}
